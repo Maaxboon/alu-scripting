927532e6136061b63c54437cb9e14ac224a97892
Maaxboon <maaxwelokoth@gmail.com>
2024-10-24 08:41:05 +0300

    Fixes PEP-8

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -7,11 +7,21 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """Returns the number of subscribers for a given subreddit."""
+    if subreddit is None or not isinstance(subreddit, str):
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/firdaus_cartoon_jr)'}
+    
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        if response.status_code != 200:
+            return 0
+        
+        data = response.json()
+        subscribers = data.get("data", {}).get("subscribers", 0)
+        return subscribers
+
+    except requests.RequestException:
+        return 0
