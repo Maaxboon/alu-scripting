23ab9a3faa354cfeacd63ea139cb7993e016f07c
Maaxboon <maaxwelokoth@gmail.com>
2024-10-25 20:02:10 +0300

    Top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """Module for task 1"""
 
+import requests
 
 def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
-    of the subreddit"""
-    import requests
+    """Queries the Reddit API and prints the titles of the top 10 hot posts
+    of the subreddit."""
+    
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "My-User-Agent"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        print('None')
+    # Check if the response is valid
+    if response.status_code == 200:
+        data = response.json().get("data", {}).get("children", [])
+        for post in data:
+            print(post.get("data", {}).get("title"))
     else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+        # Print None if subreddit does not exist
+        print("None")
