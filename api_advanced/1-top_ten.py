a82bae15d40d413072628d801ce165ed7572f95d
Maaxboon <maaxwelokoth@gmail.com>
2024-10-25 19:04:01 +0300

    Fixes PEP

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
-
-"""
-Displays the titles of 10 hot posts listed for a subreddit
-"""
-
-from requests import get
-import subprocess
+""""Doc"""
+import requests
 
 
 def top_ten(subreddit):
-    """
-    The Function that fethces the Reddit API
-    """
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    params = {'limit': 10}
-    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    """"Doc"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
 
-    response = get(url, headers=user_agent, params=params)
-    if response.status_code != 200:
-        print("None")
-        return
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
 
-    try:
-        results = response.json()
-        my_data = results.get('data').get('children')
-
-        for i in my_data:
-            print(i.get('data').get('title'))
-
-    except Exception:
-        print("None")
+    if res.status_code != 200:
+        print(None)
+    else:
+        json_response = res.json()
+        posts = json_response.get('data').get('children')
+        [print(post.get('data').get('title')) for post in posts]
