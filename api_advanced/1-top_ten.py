f328a2313dc58c4927f477ad597c38d200c7f8e5
Maaxboon <maaxwelokoth@gmail.com>
2024-10-25 20:05:07 +0300

    Top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""Module for retrieving top 10 hot posts from a subreddit."""
+"""
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
 
 import requests
 
+
 def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the top 10 hot posts
-    for a given subreddit."""
-    
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        # Check if subreddit exists by verifying a 200 status code
-        if response.status_code == 200:
-            data = response.json().get("data", {}).get("children", [])
-            # Output titles or `None` if there are no posts
-            if data:
-                for post in data:
-                    print(post.get("data", {}).get("title"))
-            else:
-                print("None")
-        else:
-            print("None")
-
-    except requests.exceptions.RequestException:
-        print("None")
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
+    else:
+        print(None)
