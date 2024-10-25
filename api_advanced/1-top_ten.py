797e135fdc926fdb739851fd254814f70d6dce74
Maaxboon <maaxwelokoth@gmail.com>
2024-10-25 09:06:15 +0300

    Makes file executable

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,11 +7,22 @@ import requests
 def top_ten(subreddit):
     """Main function"""
     URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-
     HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    
     try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        HOT_POSTS = RESPONSE.json().get("data").get("children")
-        [print(post.get('data').get('title')) for post in HOT_POSTS]
+        response = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        
+        # Check if response is successful and contains data
+        if response.status_code != 200:
+            print(None)
+            return
+        
+        hot_posts = response.json().get("data", {}).get("children", None)
+        
+        if not hot_posts:
+            print(None)
+        else:
+            for post in hot_posts:
+                print(post.get("data", {}).get("title"))
     except Exception:
         print(None)
