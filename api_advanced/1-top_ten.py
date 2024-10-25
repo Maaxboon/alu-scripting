ebad5fd269bafff9c7b94f673b0d78fc8e7e40c5
Maaxboon <maaxwelokoth@gmail.com>
2024-10-25 19:05:34 +0300

    Fixes PEP

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,20 +1,20 @@
-#!/usr/bin/python3
-""""Doc"""
 import requests
 
-
 def top_ten(subreddit):
-    """"Doc"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
-        .format(subreddit)
-
-    res = requests.get(url,
-                       headers={
-                           'User-Agent': 'Mozilla/5.0'})
-
-    if res.status_code != 200:
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json().get("data", {})
+            posts = data.get("children", [])
+            
+            # Print the title of each post
+            for post in posts:
+                print(post["data"]["title"])
+        else:
+            print(None)
+    except Exception:
         print(None)
-    else:
-        json_response = res.json()
-        posts = json_response.get('data').get('children')
-        [print(post.get('data').get('title')) for post in posts]
