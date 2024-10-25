73b4dff8e19d85b57155a321acbab21495d3725d
Maaxboon <maaxwelokoth@gmail.com>
2024-10-25 08:35:54 +0300

    Fixes output

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""Print the titles of the first 10Hot Posts"""
+"""Print the titles of the first 10 hot posts in a subreddit"""
 import requests
 
 
 def top_ten(subreddit):
-    """The top ten titles"""
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(url, headers=headers)
+    """Queries Reddit API and prints titles of the top 10 hot posts for a subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "MyAPI/0.0.1"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
+    # Check for valid response (status code 200) and parse titles
     if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                    json_data.get('data')
-                    .get('children')[i]
-                    .get('data')
-                    .get('title')
-                )
+        json_data = response.json().get("data", {}).get("children", [])
+        if json_data:
+            for post in json_data[:10]:
+                print(post["data"].get("title"))
+        else:
+            print("None")
     else:
-        print(None)
+        print("None")
